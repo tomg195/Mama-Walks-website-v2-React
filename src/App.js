2e1f8e8cd3ef7443d577c31f32d9db1d8ba64338
tomg195 <gardner195@msn.com>
2023-10-05 10:33:37 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import {
+  Accordion,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-function App() {
+import { Email } from "@mui/icons-material";
+import { Instagram } from "@mui/icons-material";
+
+const pages = ["About Us", "Walks", "Track Your Walks", "Events", "Blog"];
+
+export const AppHeader = () => {
+  const [showData, setShowData] = useState(null);
+
+  const openInstagramLink = () => {
+    window.open("https://www.instagram.com/mamawalks_/", "_blank");
+  };
+
+  const sendEmail = () => {
+    const email = "mama_walks@outlook.com";
+    window.location.href = `mailto:${email}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static" sx={{ backgroundColor: "#F5AA7B" }}>
+        <Container>
+          <Toolbar>
+            <Toolbar
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Mama Walks
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={() => {
+                      setShowData(true);
+                    }}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+                <IconButton onClick={openInstagramLink}>
+                  <Instagram />
+                </IconButton>
+                <IconButton onClick={sendEmail}>
+                  <Email />
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {showData && <div>AKAOHFHDFHSJDPJFSPODFKO</div>}
+    </>
   );
-}
-
-export default App;
+};
